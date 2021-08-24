8fce024a4f40719adc61cd4ed4250c52737de287
Adam Hoddinott <adam.hoddinott@salespreso.com>
2021-08-24 15:14:32 +1000

    complete factorial

---
+++ 01-factorial/src/main.rs
@@ -0,0 +1,42 @@
+fn get_number_from_buffer() -> u32 {
+    let result: u32;
+
+    loop {
+        let mut buffer = String::new();
+
+        std::io::stdin()
+            .read_line(&mut buffer)
+            .expect("Expected a value");
+
+        match buffer.trim().parse::<u32>() {
+            Ok(num) => {
+                result = num;
+                break;
+            }
+            Err(_) => {
+                println!("Error, not a number");
+                continue;
+            }
+        }
+    }
+
+    result
+}
+
+fn main() {
+    println!("Get the factorial of which number?");
+
+    let num = get_number_from_buffer();
+
+    //let mut factorial = 1;
+    //
+    let factorial = (1..=num).fold(1, |res, n| res * n);
+
+
+
+    //for n in 1..=num {
+    //    factorial *= n;
+    //}
+
+    println!("Factorial: {}", factorial);
+}
